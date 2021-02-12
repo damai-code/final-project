07ea5c5d110fe081389966eb56b44aa4e73eee29
awandilangitt <kurniawantrii24@gmail.com>
2021-02-12 23:35:38 +0700

    nambah-router-read

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const registerControllers = require("../controllers/registerControllers");
 
+//coba read heroku deploy
+router.get("/", (req, res) => res.send("<h1>Sportsman Homepage</h1>"));
+
 router.post("/register", registerControllers.register);
 
 module.exports = router;
