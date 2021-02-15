74067b4bcddf73a87526bb04c979569e194d65cf
triagungtio <triagungtio07@gmail.com>
2021-02-15 09:36:41 +0700

    Menambahkan category pada model tracking

--- src/models/tracking.js
+++ src/models/tracking.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Users = require("./users");
 
 const trackingSchema = new Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Users,
+  },
   time: { type: String, required: true },
   distance: { type: Number, required: true },
   speed: { type: Number, required: true },
+  category: { type: Number, required: true },
   startPoint: {
     type: {
       type: String, // Don't do `{ location: { type: String } }`
